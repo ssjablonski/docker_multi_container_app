d36ba226fbaf5b4630ba2d488413499dd19424a4
Sebastian Jablonski <s.jablonski.684@studms.ug.edu.pl>
2024-04-10 00:54:11 +0200

    minor bug fixes

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
+const OtherPage = () => {
   return (
     <div>
-      I'm some other page!
+      Im some other page!
       <Link to="/">Go back home</Link>
     </div>
   );
 };
+
+export default OtherPage;
